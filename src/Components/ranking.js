64d1e04673c7537d2456a18e596a11f03d5583ec
Kevin Li <Kevinstart14@gmail.com>
2022-07-03 17:34:02 -0300

    Boraaaa 19

--- src/Components/ranking.js
+++ src/Components/ranking.js
@@ -4,12 +4,16 @@ import PropTypes from 'prop-types';
 class RankingPlayer extends React.Component {
   render() {
     const { info, index } = this.props;
-    console.log(index);
+    // console.log(index);
     return (
       <div>
         <h3 key={ index }>{ info.player.name }</h3>
         <h4 data-testid={ `player-score-${index}` }>{ info.player.score }</h4>
-        <img alt={ info.player.gravatarImg } src={ info.player.gravatarImg } />
+        <img
+          data-testid={ `player-score-${index}` }
+          alt={ info.player.gravatarImg }
+          src={ info.player.gravatarImg }
+        />
       </div>
     );
   }
